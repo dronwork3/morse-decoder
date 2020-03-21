27eab474f3e68a3626a65415bef5e37d8b983674
Andrey Novikov <dronwork3@gmail.com>
2020-03-21 23:52:28 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,37 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+let exprArr = [];
+    for (let i = 0; i < expr.length / 10; i++) {
+      let part = expr.slice(i * 10, i * 10 + 10);
+      exprArr.push(part);
+    }
+    let morseString = '';
+    let decodeString = ''
+    for (let i = 0; i < exprArr.length; i++) {
+        for (let j = 0; j < exprArr[i].length / 2; j++) {
+            let codePair = exprArr[i].slice(j * 2, j * 2 + 2);
+            switch (codePair) {
+                case '00':
+                    break;
+                case '10':
+                    morseString += '.';
+                    break;
+                case '11':
+                    morseString += '-';
+                    break;
+            }
+
+        }
+        if (morseString) {
+            decodeString += MORSE_TABLE[morseString];
+        } else {
+            decodeString += ' '
+        };
+        morseString = '';
+    }
+
+    return decodeString;
 }
 
 module.exports = {
